033efbeb4303385ace38ede47aad70c1fcaa4b38
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-07-01 11:30:25 +0500

    Личный проект: перламутровые пуговицы (часть 2)

--- js/filter-form.js
+++ js/filter-form.js
@@ -2,36 +2,78 @@
 (function () {
   var filterForm = document.querySelector('.map__filters');
   var mapFilterElements = document.querySelectorAll('.map__filters > *');
-  var houseTypeSelect = document.querySelector('#housing-type');
 
-  var loadedOffers;
+  var filterSelects = filterForm.querySelectorAll('select.map__filter');
+  var filterCheckboxes = filterForm.querySelectorAll('input.map__checkbox');
 
-  var filterOffers = function () {
-    var type = houseTypeSelect.value;
-    if (type !== 'any') {
-      var filteredOffers = loadedOffers.filter(function (offer) {
-        return offer.offer.type === type;
-      });
-      return filteredOffers;
+  var loadedOffers = [];
+  var filteredOffers = [];
+
+  var filterByPrice = function (price, range) {
+    var result;
+    switch (range) {
+      case 'low':
+        result = price < 10000;
+        break;
+      case 'middle':
+        result = price >= 10000 && price <= 50000;
+        break;
+      case 'high':
+        result = price > 50000;
+        break;
     }
-    return loadedOffers;
+    return result;
   };
 
-  var onHouseTypeChange = function () {
-    window.map.updatePins(filterOffers());
+  var filterOffers = function () {
+    filteredOffers = loadedOffers;
+
+    filterSelects.forEach(function (select) {
+      if (select.value === 'any') {
+        return;
+      }
+
+      if (select.name === 'housing-price') {
+        filteredOffers = filteredOffers.filter(function (item) {
+          return filterByPrice(item.offer.price, select.value);
+        });
+      } else {
+        filteredOffers = filteredOffers.filter(function (item) {
+          var value = select.name.split('-')[1];
+          return item.offer[value].toString() === select.value;
+        });
+      }
+    });
+
+    filterCheckboxes.forEach(function (checkbox) {
+      if (!checkbox.checked) {
+        return;
+      }
+
+      filteredOffers = filteredOffers.filter(function (item) {
+        return item.offer.features.indexOf(checkbox.value) !== -1;
+      });
+    });
+
+    return filteredOffers;
+  };
+
+  var onFilterFormChange = function (evt) {
+    window.map.updatePins(filterOffers(evt));
   };
 
   var init = function (offers) {
     loadedOffers = offers;
+
     window.util.enableFormElements(mapFilterElements);
-    houseTypeSelect.addEventListener('change', onHouseTypeChange);
+    filterForm.addEventListener('change', onFilterFormChange);
     window.map.updatePins(filterOffers());
   };
 
   var destroy = function () {
     window.util.disableFormElements(mapFilterElements);
     filterForm.reset();
-    houseTypeSelect.removeEventListener('change', onHouseTypeChange);
+    filterForm.removeEventListener('change', onFilterFormChange);
   };
 
   window.filterForm = {
